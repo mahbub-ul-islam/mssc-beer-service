b7de9095d10c2ed724c6ec3253b83e304c974373
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-23 06:07:56 +0600

    addind bootstrap. closes #10

---
+++ src/main/java/com/learning/beerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,49 @@
+package com.learning.beerservice.bootstrap;
+
+import com.learning.beerservice.domain.Beer;
+import com.learning.beerservice.repositories.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public BeerLoader(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (repository.count() == 0) {
+
+            repository.save(Beer.builder()
+                            .beerName("Mango Bobs")
+                            .beerStyle("IPA")
+                            .quantityToBrew(200)
+                            .minOnHand(12)
+                            .upc(337010000001L)
+                            .price(new BigDecimal("12.95"))
+                            .build());
+
+            repository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+//        System.out.println("Loaded Beers: " + repository.count());
+    }
+}
